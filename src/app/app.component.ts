d56411219b3196ff9ee7d987da1223b6d4a5f89e
s21sd <srivastavasunny359@gmail.com>
2024-02-02 22:08:18 +0530

    "More Learinig"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { AuthModule } from '../app/auth/auth.module'
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
 @Component({
   selector: 'hinv-root',
   standalone: true,
-  imports: [RouterOutlet, UserListComponent, AuthModule],
+  imports: [RouterOutlet, UserListComponent, AuthModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,5 +17,19 @@ export class AppComponent {
 
   getName(name: string) {
     console.log(name);
+  }
+  count = 0;
+  Increase(type: string) {
+    if (type === 'add') {
+      this.count++;
+    }
+    else if (type === 'minus') {
+      this.count--;
+    }
+  }
+
+  show = true
+  showDiv() {
+    this.show = !this.show
   }
 }
